3d4c70cedb80b65d3ced4970a78deebcfb8a0037
GitHub Action <action@github.com>
2025-08-25 15:06:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/jamie10x/kobwebplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jamie10x/kobwebplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jamie10x.kobwebplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jamie10x.kobwebplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
